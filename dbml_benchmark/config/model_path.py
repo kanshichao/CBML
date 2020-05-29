05188e747cb0d189ae6ca1aaafa7a71b5af7d46c
kanshichao <16112062@bjtu.edu.cn>
2020-05-29 00:02:10 -0500

    add googlenet model path

--- dbml_benchmark/config/model_path.py
+++ dbml_benchmark/config/model_path.py
@@ -4,6 +4,7 @@ from yacs.config import CfgNode as CN
 MODEL_PATH = {
     'bninception': "~/.cache/torch/checkpoints/bn_inception-52deb4733.pth",
     'resnet50': "~/.cache/torch/checkpoints/resnet50-19c8e357.pth",
+    'googlenet': "~/.cache/torch/checkpoints/googlenet-1378be20.pth"
 }
 
 MODEL_PATH = CN(MODEL_PATH)
